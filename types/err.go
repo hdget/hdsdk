e2845fc69e1cfb7a527e6d5e6ad0c4d5ee84231a
Ryan Fan <reg_info@qq.com>
2021-09-15 14:05:28 +0800

    增加错误定义

--- types/err.go
+++ types/err.go
@@ -3,9 +3,6 @@ package types
 import "errors"
 
 var (
-	ErrMissingValue  = errors.New("(MISSING)")
 	ErrEmptyConfig   = errors.New("empty config")
 	ErrInvalidConfig = errors.New("invalid config")
-	ErrNoProvider    = errors.New("provider not found")
-	ErrNoCapability  = errors.New("capability not defined")
 )
