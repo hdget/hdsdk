d999960aaa8ba7660dcebf06c21e6aa637d83e66
Ryan Fan <reg_info@qq.com>
2021-09-23 12:10:27 +0800

    ws add router group support

--- lib/ws/server.go
+++ lib/ws/server.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"github.com/gin-gonic/gin"
 	"github.com/hdget/hdsdk"
+	"github.com/hdget/hdsdk/lib/err"
 	"github.com/hdget/hdsdk/types"
 	"github.com/hdget/hdsdk/utils/parallel"
 	"net/http"
@@ -13,7 +14,8 @@ import (
 
 type HttpServer struct {
 	*http.Server
-	router *gin.Engine
+	router       *gin.Engine
+	routerGroups map[string]*gin.RouterGroup
 }
 
 type HttpMethod int
@@ -25,6 +27,11 @@ const (
 )
 const waitTime = 3 * time.Second
 
+var (
+	ErrDuplicateRouterGroup = err.New("duplicate router group")
+	ErrRouterGroupNotFound  = err.New("router group not found")
+)
+
 func NewHttpServer(logger types.LogProvider, address string) *HttpServer {
 	router := NewRouter(logger)
 	return &HttpServer{
@@ -32,7 +39,8 @@ func NewHttpServer(logger types.LogProvider, address string) *HttpServer {
 			Addr:    address,
 			Handler: router,
 		},
-		router: router,
+		router:       router,
+		routerGroups: make(map[string]*gin.RouterGroup),
 	}
 }
 
@@ -74,23 +82,38 @@ func (srv *HttpServer) Run() {
 	}
 }
 
-// AddRoute add route handler
-func (srv *HttpServer) AddRoute(method HttpMethod, path string, handler gin.HandlerFunc) {
-	switch method {
-	case Get:
-		srv.router.GET(path, handler)
-	case Post:
-		srv.router.POST(path, handler)
-	case Delete:
-		srv.router.DELETE(path, handler)
+// CreateRouterGroup create a gin router group
+func (srv *HttpServer) CreateRouterGroup(name, relativePath string, handlers ...gin.HandlerFunc) error {
+	_, exist := srv.routerGroups[name]
+	if exist {
+		return ErrDuplicateRouterGroup
+	}
+
+	srv.routerGroups[name] = srv.router.Group(relativePath, handlers...)
+	return nil
+}
+
+func (srv *HttpServer) GetRouterGroup(groupName string) *gin.RouterGroup {
+	return srv.routerGroups[groupName]
+}
+
+// AddRoutes add routes from Route slice
+func (srv *HttpServer) AddRoutes(routes []Route) {
+	for _, r := range routes {
+		addToRouter(srv.router, r.Method, r.Path, r.Handler)
 	}
 }
 
-// SetupRoutes setup routes from Route slice
-func (srv *HttpServer) SetupRoutes(routes []Route) {
-	for _, r := range routes {
-		srv.AddRoute(r.Method, r.Path, r.Handler)
+func (srv *HttpServer) AddGroupRoutes(groupName string, routes []Route) error {
+	routerGroup := srv.GetRouterGroup(groupName)
+	if routerGroup == nil {
+		return ErrRouterGroupNotFound
 	}
+
+	for _, r := range routes {
+		addToRouterGroup(routerGroup, r.Method, r.Path, r.Handler)
+	}
+	return nil
 }
 
 func (srv *HttpServer) shutdown(err error) {
@@ -98,5 +121,27 @@ func (srv *HttpServer) shutdown(err error) {
 	defer cancel()
 	if err := srv.Shutdown(ctx); err != nil {
 		hdsdk.Logger.Fatal("http server shutdown", "error", err)
+	}
+}
+
+func addToRouterGroup(routerGroup *gin.RouterGroup, method HttpMethod, path string, handler gin.HandlerFunc) {
+	switch method {
+	case Get:
+		routerGroup.GET(path, handler)
+	case Post:
+		routerGroup.POST(path, handler)
+	case Delete:
+		routerGroup.DELETE(path, handler)
+	}
+}
+
+func addToRouter(router *gin.Engine, method HttpMethod, path string, handler gin.HandlerFunc) {
+	switch method {
+	case Get:
+		router.GET(path, handler)
+	case Post:
+		router.POST(path, handler)
+	case Delete:
+		router.DELETE(path, handler)
 	}
 }
