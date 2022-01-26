05cbc65a91a1372e6016abd1bcb23c6d60ee969c
Ryan Fan <reg_info@qq.com>
2022-01-26 18:03:04 +0800

    change dapr functions

--- lib/dapr/root.go
+++ lib/dapr/root.go
@@ -8,7 +8,8 @@ import (
 	"github.com/pkg/errors"
 )
 
-func InvokeService(appId, methodName string, data interface{}) ([]byte, error) {
+// InvokeOnce 调用一次
+func InvokeOnce(appId, methodName string, data interface{}) ([]byte, error) {
 	var value []byte
 	switch t := data.(type) {
 	case string:
@@ -25,6 +26,7 @@ func InvokeService(appId, methodName string, data interface{}) ([]byte, error) {
 	if err != nil {
 		return nil, errors.Wrap(err, "new dapr client")
 	}
+	defer daprClient.Close()
 
 	content := &client.DataContent{
 		ContentType: "application/json",
@@ -37,4 +39,31 @@ func InvokeService(appId, methodName string, data interface{}) ([]byte, error) {
 	}
 
 	return resp, nil
+}
+
+// Invoke 需要传入daprClient去调用
+func Invoke(daprClient client.Client, appId, methodName string, data interface{}) ([]byte, error) {
+	var value []byte
+	switch t := data.(type) {
+	case string:
+		value = utils.StringToBytes(t)
+	default:
+		v, err := json.Marshal(data)
+		if err != nil {
+			return nil, errors.Wrap(err, "marshal invoke data")
+		}
+		value = v
+	}
+
+	content := &client.DataContent{
+		ContentType: "application/json",
+		Data:        value,
+	}
+
+	ret, err := daprClient.InvokeMethodWithContent(context.Background(), appId, methodName, "post", content)
+	if err != nil {
+		return nil, errors.Wrapf(err, "dapr invoke method with content, app:%s, method: %s, content: %s", appId, methodName, utils.BytesToString(value))
+	}
+
+	return ret, nil
 }
