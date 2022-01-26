63a79d4da3f3b27796bcc8622296d56333999986
Ryan Fan <reg_info@qq.com>
2022-01-26 18:39:58 +0800

    skip log some special http methods

--- lib/ws/middleware.go
+++ lib/ws/middleware.go
@@ -3,12 +3,27 @@ package ws
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/hdget/hdsdk/types"
+	"github.com/hdget/hdsdk/utils"
 	"strings"
 	"time"
 )
 
+var SkipHttpMethods = []string{
+	"PRI",
+	"HEAD",
+}
+
 func mdwLogger(logger types.LogProvider) gin.HandlerFunc {
 	return func(c *gin.Context) {
+		//请求方式
+		reqMethod := c.Request.Method
+		if utils.StringSliceContains(SkipHttpMethods, reqMethod) {
+			c.Next()
+			return
+		}
+
+		//请求路由
+		reqUrl := c.Request.RequestURI
 		//开始时间
 		startTime := time.Now()
 		//处理请求
@@ -17,10 +32,7 @@ func mdwLogger(logger types.LogProvider) gin.HandlerFunc {
 		endTime := time.Now()
 		// 执行时间
 		latencyTime := endTime.Sub(startTime)
-		//请求方式
-		reqMethod := c.Request.Method
-		//请求路由
-		reqUrl := c.Request.RequestURI
+
 		//状态码
 		statusCode := c.Writer.Status()
 		//请求ip
