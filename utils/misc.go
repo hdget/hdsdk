a487d539a559b8e3bd79f022fdddabd273800bb3
Ryan Fan <reg_info@qq.com>
2022-03-25 10:36:58 +0800

    float to fixed

--- utils/misc.go
+++ utils/misc.go
@@ -130,3 +130,13 @@ func IsImageData(data []byte) bool {
 	}
 	return false
 }
+
+func round(num float64) int {
+	return int(num + math.Copysign(0.5, num))
+}
+
+// ToFixed 浮点数到指定小数位
+func ToFixed(num float64, precision int) float64 {
+	output := math.Pow(10, float64(precision))
+	return float64(round(num*output)) / output
+}
