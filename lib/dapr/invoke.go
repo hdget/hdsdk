4610166d363bac8a819ff7e0ea08b09b5833bdd3
Ryan Fan <reg_info@qq.com>
2022-04-09 13:52:52 +0800

    upgrade invoke

--- lib/dapr/invoke.go
+++ lib/dapr/invoke.go
@@ -3,6 +3,7 @@ package dapr
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"github.com/dapr/go-sdk/client"
 	"github.com/dapr/go-sdk/service/common"
 	"github.com/hdget/hdsdk/utils"
@@ -20,54 +21,20 @@ const ContentTypeJson = "application/json"
 //}
 //var jsonpbMarshaler = jsonpb.Marshaler{EmitDefaults: true}
 
-// InvokeService 调用dapr服务
-func InvokeService(appId, methodName string, data interface{}, args ...string) ([]byte, error) {
-	var value []byte
-	switch t := data.(type) {
-	case string:
-		value = utils.StringToBytes(t)
-	case []byte:
-		value = t
-	default:
-		v, err := json.Marshal(data)
-		if err != nil {
-			return nil, errors.Wrap(err, "marshal invoke data")
-		}
-		value = v
-	}
-
-	daprClient, err := client.NewClient()
-	if err != nil {
-		return nil, errors.Wrap(err, "new dapr client")
-	}
-	if daprClient == nil {
-		return nil, errors.New("dapr client is null, name resolution service may not started, please check it")
-	}
-
-	// IMPORTANT: daprClient是全局的连接, 不能关闭
-	//defer daprClient.Close()
-	// 添加额外的meta信息
-	ctx := context.Background()
-	if len(args) > 0 {
-		md := metadata.Pairs(args...)
-		ctx = metadata.NewOutgoingContext(ctx, md)
-	}
-
-	content := &client.DataContent{
-		ContentType: "application/json",
-		Data:        value,
-	}
-
-	resp, err := daprClient.InvokeMethodWithContent(ctx, appId, methodName, "post", content)
-	if err != nil {
-		return nil, errors.Wrapf(err, "dapr invoke method with content, app:%s, method: %s, content: %s", appId, methodName, utils.BytesToString(value))
-	}
-
-	return resp, nil
+// Invoke 调用dapr服务
+func Invoke(version int, appId, namespace, method string, data interface{}, args ...string) ([]byte, error) {
+	fullMethodName := getFullMethodName(version, namespace, "", method)
+	return realInvoke(appId, fullMethodName, data, args...)
 }
 
-// InvokeServiceWithClient 需要传入daprClient去调用
-func InvokeServiceWithClient(daprClient client.Client, appId, methodName string, data interface{}, args ...string) ([]byte, error) {
+// InvokeWithClient 调用dapr服务
+func InvokeWithClient(version int, appId, namespace, client, method string, data interface{}, args ...string) ([]byte, error) {
+	fullMethodName := getFullMethodName(version, namespace, client, method)
+	return realInvoke(appId, fullMethodName, data, args...)
+}
+
+// InvokeWithDaprClient 需要传入daprClient去调用
+func InvokeWithDaprClient(daprClient client.Client, appId, methodName string, data interface{}, args ...string) ([]byte, error) {
 	if daprClient == nil {
 		return nil, errors.New("dapr client is null, name resolution service may not started, please check it")
 	}
@@ -104,6 +71,51 @@ func InvokeServiceWithClient(daprClient client.Client, appId, methodName string,
 	}
 
 	return ret, nil
+}
+
+func realInvoke(appId, fullMethodName string, data interface{}, args ...string) ([]byte, error) {
+	var value []byte
+	switch t := data.(type) {
+	case string:
+		value = utils.StringToBytes(t)
+	case []byte:
+		value = t
+	default:
+		v, err := json.Marshal(data)
+		if err != nil {
+			return nil, errors.Wrap(err, "marshal invoke data")
+		}
+		value = v
+	}
+
+	daprClient, err := client.NewClient()
+	if err != nil {
+		return nil, errors.Wrap(err, "new dapr client")
+	}
+	if daprClient == nil {
+		return nil, errors.New("dapr client is null, name resolution service may not started, please check it")
+	}
+
+	// IMPORTANT: daprClient是全局的连接, 不能关闭
+	//defer daprClient.Close()
+	// 添加额外的meta信息
+	ctx := context.Background()
+	if len(args) > 0 {
+		md := metadata.Pairs(args...)
+		ctx = metadata.NewOutgoingContext(ctx, md)
+	}
+
+	content := &client.DataContent{
+		ContentType: "application/json",
+		Data:        value,
+	}
+
+	resp, err := daprClient.InvokeMethodWithContent(ctx, appId, fullMethodName, "post", content)
+	if err != nil {
+		return nil, errors.Wrapf(err, "dapr invoke method with content, app:%s, method: %s, content: %s", appId, fullMethodName, utils.BytesToString(value))
+	}
+
+	return resp, nil
 }
 
 // GetMetaValues get grpc meta values
@@ -159,4 +171,16 @@ func getNamespaceName(myvar interface{}) string {
 	} else {
 		return t.Name()
 	}
+}
+
+// getFullMethodName 构造version:namespace:client:realMethod的方法名
+// 为了进行namespace和版本号的区分，组装method=version:moduleName:clientName:realMethod
+// 其中client可能为空，这个说明该接口可以给任何client使用
+func getFullMethodName(version int, namespace, client, method string) string {
+	tokens := []string{fmt.Sprintf("v%d", version), namespace, method}
+	if client != "" {
+		tokens = []string{fmt.Sprintf("v%d", version), namespace, method}
+	}
+
+	return strings.Join(tokens, ":")
 }
