2fed8b72024c54292d879e27e65fca2c07f075b4
hdget <87684157+hdget@users.noreply.github.com>
2023-03-03 16:01:35 +0800

    fix duplicate handler issue (#10)
    Co-authored-by: rfancn <reg_info@qq.com>

--- lib/dapr/handler.go
+++ lib/dapr/handler.go
@@ -2,8 +2,10 @@ package dapr
 
 import (
 	"context"
+	"fmt"
 	"github.com/dapr/go-sdk/service/common"
 	"github.com/hdget/hdsdk/utils"
+	"strings"
 )
 
 // serviceModuleHandler 服务模块的方法信息
@@ -13,14 +15,25 @@ type serviceModuleHandler struct {
 	invocationHandler common.ServiceInvocationHandler // dapr.ServiceInvocationHandler实例
 }
 
-func (sm *ServiceModule) registerHandlers(methods map[string]common.ServiceInvocationHandler) {
+func (sm *ServiceModule) registerHandlers(methods map[string]common.ServiceInvocationHandler) error {
 	for methodName, handler := range methods {
-		sm.handlers[utils.GetFuncName(handler)] = &serviceModuleHandler{
+		k := sm.getFullHandlerName(utils.GetFuncName(handler))
+		if _, exist := sm.handlers[k]; exist {
+			return fmt.Errorf("duplicate handler registered, handler: %s", k)
+		}
+
+		sm.handlers[k] = &serviceModuleHandler{
 			name:              methodName,
 			method:            getFullMethodName(sm.version, sm.namespace, sm.client, methodName),
 			invocationHandler: wrapRecoverHandler(sm.app, handler), // 封装recover
 		}
 	}
+	return nil
+}
+
+// getFullHandlerName 获取完整的receiver.funcName
+func (sm ServiceModule) getFullHandlerName(handlerName string) string {
+	return strings.Join([]string{sm.name, handlerName}, "_")
 }
 
 // wrapRecoverHandler 将panic recover处理逻辑封装进去
