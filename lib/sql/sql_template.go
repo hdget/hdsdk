e39b19dc81ab0b07af0410a093add5de207a05d4
hdget <87684157+hdget@users.noreply.github.com>
2023-05-19 17:22:56 +0800

    Feat/add join subquery (#17)
    * add get wheres
    
    * reconstruct sql template
    
    * rename
    
    ---------
    
    Co-authored-by: rfancn <reg_info@qq.com>

--- lib/sql/sql_template.go
+++ lib/sql/sql_template.go
@@ -10,8 +10,28 @@ import (
 	"strings"
 )
 
-// sqlTemplate 基于模板创建, 只支持where和limit
-type sqlTemplate struct {
+// SqlTemplate 基于模板创建, 只支持where和limit
+type SqlTemplate interface {
+	With(template string) SqlTemplate
+	Limit(listParam *protobuf.ListParam) SqlTemplate
+	Next(pkName string, nextParam *protobuf.NextParam) SqlTemplate
+	OrderBy(orderBy string) SqlTemplate
+	GroupBy(groupBy string) SqlTemplate
+	InsertArgs(extraArgs ...any) SqlTemplate
+	AppendArgs(extraArgs ...any) SqlTemplate
+	JoinSubQuery(subQuery *joinSubQuery) SqlTemplate
+
+	Where(condition string, args ...any)
+	Generate() (string, []any, error)
+	Get(v any) error
+	Select(v any) error
+	Count() (int64, error)
+	ExportConditions() ([]string, []any) // 导出where条件
+	ImportConditions([]string, []any)    // 导入where条件
+}
+
+type baseQuery struct {
+	concrete    SqlTemplate
 	template    string
 	wheres      []string
 	args        []any
@@ -20,144 +40,247 @@ type sqlTemplate struct {
 	groupBy     string
 }
 
+type joinSubQuery struct {
+	*baseQuery
+	alias string
+	on    string
+}
+
+// SqlTemplate 基于模板创建, 只支持where和limit
+type query struct {
+	*baseQuery
+	joinSubQuerys []*joinSubQuery
+}
+
 const (
 	defaultNextClause  = "LIMIT 10"
 	defaultLimitClause = "LIMIT 0, 10"
 )
 
-func NewSqlTemplate() *sqlTemplate {
-	return &sqlTemplate{
-		wheres: make([]string, 0),
-		args:   make([]any, 0),
+func NewSqlTemplate() SqlTemplate {
+	me := &query{
+		baseQuery: &baseQuery{
+			wheres: make([]string, 0),
+			args:   make([]any, 0),
+		},
+	}
+	me.concrete = me
+	return me
+}
+
+func NewJoinSubQuery(template, alias, on string) *joinSubQuery {
+	return &joinSubQuery{
+		baseQuery: &baseQuery{
+			template: template,
+			wheres:   make([]string, 0),
+			args:     make([]any, 0),
+		},
+		alias: alias,
+		on:    on,
 	}
 }
 
-func (h *sqlTemplate) Where(condition string, args ...any) {
-	h.wheres = append(h.wheres, fmt.Sprintf("(%s)", condition))
-	h.args = append(h.args, args...)
+/* query */
+
+func (q *query) JoinSubQuery(subQuery *joinSubQuery) SqlTemplate {
+	q.joinSubQuerys = append(q.joinSubQuerys, subQuery)
+	return q
 }
 
-func (h *sqlTemplate) With(template string) *sqlTemplate {
-	h.template = template
-	return h
+// Generate 最终生成SQL语句
+func (q *query) Generate() (string, []any, error) {
+	parts := []string{q.template}
+
+	// join子查询
+	for _, sq := range q.joinSubQuerys {
+		subQuerySql, subQueryArgs, err := sq.Generate()
+		if err != nil {
+			return "", nil, err
+		}
+
+		parts = append(parts, fmt.Sprintf("INNER JOIN (%s) AS %s ON %s", subQuerySql, sq.alias, sq.on))
+		q.InsertArgs(subQueryArgs...)
+	}
+
+	whereClause := q.getWhereClause()
+	if whereClause != "" {
+		parts = append(parts, whereClause)
+	}
+
+	if q.groupBy != "" {
+		parts = append(parts, q.groupBy)
+	}
+
+	if q.orderBy != "" {
+		parts = append(parts, q.orderBy)
+	}
+
+	if q.limitClause != "" {
+		parts = append(parts, q.limitClause)
+	}
+
+	return q.process(strings.Join(parts, " "))
 }
 
-func (h *sqlTemplate) Limit(listParam *protobuf.ListParam) *sqlTemplate {
+/* joinSubQuery */
+
+func (sq *joinSubQuery) Generate() (string, []any, error) {
+	parts := []string{sq.template}
+
+	whereClause := sq.getWhereClause()
+	if whereClause != "" {
+		parts = append(parts, whereClause)
+	}
+
+	if sq.groupBy != "" {
+		parts = append(parts, sq.groupBy)
+	}
+
+	if sq.orderBy != "" {
+		parts = append(parts, sq.orderBy)
+	}
+
+	if sq.limitClause != "" {
+		parts = append(parts, sq.limitClause)
+	}
+
+	return sq.process(strings.Join(parts, " "))
+}
+
+/* baseQuery */
+
+func (b *baseQuery) Where(condition string, args ...any) {
+	b.wheres = append(b.wheres, fmt.Sprintf("(%s)", condition))
+	b.args = append(b.args, args...)
+}
+
+func (b *baseQuery) ExportConditions() ([]string, []any) {
+	return b.wheres, b.args
+}
+
+func (b *baseQuery) ImportConditions(wheres []string, args []any) {
+	b.wheres, b.args = wheres, args
+}
+
+func (b *baseQuery) With(template string) SqlTemplate {
+	b.template = template
+	return b
+}
+
+func (b *baseQuery) Limit(listParam *protobuf.ListParam) SqlTemplate {
 	if listParam != nil {
-		h.limitClause = pagination.New(listParam.Page, listParam.PageSize).GetLimitClause()
-		return h
+		b.limitClause = pagination.New(listParam.Page, listParam.PageSize).GetLimitClause()
+		return b
 	}
-	h.limitClause = defaultLimitClause
-	return h
+	b.limitClause = defaultLimitClause
+	return b
 }
 
-func (h *sqlTemplate) Next(pkName string, nextParam *protobuf.NextParam) *sqlTemplate {
+func (b *baseQuery) Next(pkName string, nextParam *protobuf.NextParam) SqlTemplate {
 	if nextParam == nil {
-		h.limitClause = defaultNextClause
-		return h
+		b.limitClause = defaultNextClause
+		return b
 	}
 
 	if nextParam.LastPk > 0 {
 		switch nextParam.Direction {
 		case protobuf.SortDirection_Desc:
-			h.Where(fmt.Sprintf("%s < %d", pkName, nextParam.LastPk))
+			b.Where(fmt.Sprintf("%s < %d", pkName, nextParam.LastPk))
 		default:
-			h.Where(fmt.Sprintf("%s > %d", pkName, nextParam.LastPk))
+			b.Where(fmt.Sprintf("%s > %d", pkName, nextParam.LastPk))
 		}
 	}
 
-	h.limitClause = fmt.Sprintf("LIMIT %d", nextParam.PageSize)
-	return h
+	b.limitClause = fmt.Sprintf("LIMIT %d", nextParam.PageSize)
+	return b
 }
 
-func (h *sqlTemplate) OrderBy(orderBy string) *sqlTemplate {
-	h.orderBy = fmt.Sprintf("ORDER BY %s", orderBy)
-	return h
+func (b *baseQuery) OrderBy(orderBy string) SqlTemplate {
+	b.orderBy = fmt.Sprintf("ORDER BY %s", orderBy)
+	return b
 }
 
-func (h *sqlTemplate) GroupBy(groupBy string) *sqlTemplate {
-	h.groupBy = fmt.Sprintf("GROUP BY %s", groupBy)
-	return h
+func (b *baseQuery) GroupBy(groupBy string) SqlTemplate {
+	b.groupBy = fmt.Sprintf("GROUP BY %s", groupBy)
+	return b
 }
 
-func (h *sqlTemplate) Get(v any) error {
-	query, args, err := h.Generate()
+func (b *baseQuery) InsertArgs(extraArgs ...any) SqlTemplate {
+	b.args = append(extraArgs, b.args...)
+	return b
+}
+
+func (b *baseQuery) AppendArgs(extraArgs ...any) SqlTemplate {
+	b.args = append(b.args, extraArgs...)
+	return b
+}
+
+func (b *baseQuery) JoinSubQuery(subQuery *joinSubQuery) SqlTemplate {
+	return b.concrete.JoinSubQuery(subQuery)
+}
+
+func (b *baseQuery) Generate() (string, []any, error) {
+	return b.concrete.Generate()
+}
+
+func (b *baseQuery) Get(v any) error {
+	xquery, xargs, err := b.concrete.Generate()
 	if err != nil {
-		return errors.Wrap(err, "build query")
+		return errors.Wrap(err, "generate sql")
 	}
 
-	err = hdsdk.Mysql.My().Get(&v, query, args...)
+	err = hdsdk.Mysql.My().Get(v, xquery, xargs...)
 	if err != nil {
 		return errors.Wrap(err, "db get")
 	}
 	return nil
 }
 
-// Generate 最终生成SQL语句
-func (h *sqlTemplate) Generate() (string, []any, error) {
-	parts := []string{h.template}
-
-	whereClause := h.getWhereClause()
-	if whereClause != "" {
-		parts = append(parts, whereClause)
+func (b *baseQuery) Select(v any) error {
+	xquery, xargs, err := b.concrete.Generate()
+	if err != nil {
+		return errors.Wrap(err, "generate sql")
 	}
 
-	if h.groupBy != "" {
-		parts = append(parts, h.groupBy)
+	err = hdsdk.Mysql.My().Select(v, xquery, xargs...)
+	if err != nil {
+		return errors.Wrap(err, "db select")
 	}
-
-	if h.orderBy != "" {
-		parts = append(parts, h.orderBy)
-	}
-
-	if h.limitClause != "" {
-		parts = append(parts, h.limitClause)
-	}
-
-	return h.process(strings.Join(parts, " "))
+	return nil
 }
 
-func (h *sqlTemplate) Count() (int64, error) {
+func (b *baseQuery) Count() (int64, error) {
 	// 获取total
-	query, args, err := h.Generate()
+	xquery, xargs, err := b.concrete.Generate()
 	if err != nil {
-		return 0, errors.Wrap(err, "build query")
+		return 0, errors.Wrap(err, "generate sql")
 	}
+
 	var total int64
-	err = hdsdk.Mysql.My().Get(&total, query, args...)
+	err = hdsdk.Mysql.My().Get(&total, xquery, xargs...)
 	if err != nil {
 		return 0, errors.Wrap(err, "db count")
 	}
 	return total, nil
 }
 
-func (h *sqlTemplate) InsertArgs(extraArgs ...any) *sqlTemplate {
-	h.args = append(extraArgs, h.args...)
-	return h
-}
-
-func (h *sqlTemplate) AppendArgs(extraArgs ...any) *sqlTemplate {
-	h.args = append(h.args, extraArgs...)
-	return h
-}
-
-func (h *sqlTemplate) getWhereClause() string {
-	if len(h.wheres) == 0 {
+func (b *baseQuery) getWhereClause() string {
+	if len(b.wheres) == 0 {
 		return ""
 	}
-	return fmt.Sprintf("WHERE %s", strings.Join(h.wheres, " AND "))
+	return fmt.Sprintf("WHERE %s", strings.Join(b.wheres, " AND "))
 }
 
 // process 后期处理，现在暂时只处理SQL中的IN关键字
-func (h *sqlTemplate) process(query string) (string, []any, error) {
+func (b *baseQuery) process(query string) (string, []any, error) {
 	// 发现如果where里面有IN, 需要特殊处理
 	if strings.Contains(strings.ToUpper(query), " IN ") {
-		newQuery, newArgs, err := sqlx.In(query, h.args...)
+		newQuery, newArgs, err := sqlx.In(query, b.args...)
 		if err != nil {
 			return "", nil, errors.Wrap(err, "sqlx.In")
 		}
 		newQuery = hdsdk.Mysql.My().Rebind(newQuery)
 		return newQuery, newArgs, nil
 	}
-	return query, h.args, nil
+	return query, b.args, nil
 }
