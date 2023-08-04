01c5f7c0945fd76a0c7cf8fcde9ef0ec558cab51
hdget <87684157+hdget@users.noreply.github.com>
2023-08-04 13:01:03 +0800

    add clean str (#23)
    * add clean str
    
    * add clean str
    
    ---------
    
    Co-authored-by: rfancn <reg_info@qq.com>

--- utils/string.go
+++ utils/string.go
@@ -1,9 +1,31 @@
 package utils
 
 import (
+	"regexp"
 	"strings"
 	"unicode"
 )
+
+var (
+	nonNumericRegex      = regexp.MustCompile(`[^0-9 ]+`)       // 非数字
+	nonAlphanumericRegex = regexp.MustCompile(`[^a-zA-Z0-9 ]+`) // 非英文字符和数字
+	nonChineseRegex      = regexp.MustCompile(`[^\p{Han}]+`)    // 非汉字
+)
+
+// Numeric 去除数字以外的所有字符
+func Numeric(s string) string {
+	return nonNumericRegex.ReplaceAllString(s, "")
+}
+
+// AlphaNumeric 去除字符数字以外的所有字符
+func AlphaNumeric(s string) string {
+	return nonAlphanumericRegex.ReplaceAllString(s, "")
+}
+
+// Chinese 去除中文以外的所有字符
+func Chinese(s string) string {
+	return nonChineseRegex.ReplaceAllString(s, "")
+}
 
 // CleanString 处理字符串, args[0]为是否转换为小写
 func CleanString(origStr string, args ...bool) string {
