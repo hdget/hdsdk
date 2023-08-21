9b2467f419743452b982bf43815aa7d2958f6625
rfancn <reg_info@qq.com>
2023-08-21 18:31:29 +0800

    move common stuff to sdk

---
+++ lib/svc/service_dapr.go
@@ -0,0 +1,80 @@
+package svc
+
+import (
+	"github.com/dapr/go-sdk/service/common"
+	"github.com/hdget/hdsdk/lib/dapr"
+	"github.com/pkg/errors"
+)
+
+type DaprService interface {
+	Service
+	GetInvocationHandlers() map[string]common.ServiceInvocationHandler
+	GetBindingHandlers() map[string]common.BindingInvocationHandler
+	GetEvents() []dapr.Event
+}
+
+type hdDaprService struct {
+}
+
+func NewDaprService() DaprService {
+	return &hdDaprService{}
+}
+
+func (impl *hdDaprService) GetInvocationHandlers() map[string]common.ServiceInvocationHandler {
+	// 获取handlers
+	handlers := make(map[string]common.ServiceInvocationHandler)
+	for _, module := range GetRegistry() {
+		for name, anyHandler := range module.GetHandlers() {
+			handler, ok := anyHandler.(common.ServiceInvocationHandler)
+			if ok {
+				handlers[name] = handler
+			}
+		}
+	}
+
+	return handlers
+}
+
+// Initialize 初始化server
+func (impl *hdDaprService) Initialize(server any, generators ...Generator) error {
+	daprServer, ok := server.(common.Service)
+	if !ok {
+		return errors.New("invalid dapr common.service")
+	}
+
+	for method, handler := range impl.GetInvocationHandlers() {
+		if err := daprServer.AddServiceInvocationHandler(method, handler); err != nil {
+			return errors.Wrap(err, "adding invocation handler")
+		}
+	}
+
+	for name, handler := range impl.GetBindingHandlers() {
+		if err := daprServer.AddBindingInvocationHandler(name, handler); err != nil {
+			return errors.Wrap(err, "adding binding handler")
+		}
+	}
+
+	for _, event := range impl.GetEvents() {
+		if err := daprServer.AddTopicEventHandler(event.Subscription, event.Handler); err != nil {
+			return errors.Wrap(err, "adding event handler")
+		}
+	}
+
+	// 注册生成的依赖文件
+	for _, gen := range generators {
+		err := gen.Register()
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (impl *hdDaprService) GetBindingHandlers() map[string]common.BindingInvocationHandler {
+	return nil
+}
+
+func (impl *hdDaprService) GetEvents() []dapr.Event {
+	return nil
+}
