564c35be33a69c3f434c0ae91bec5b82126dc0fe
rfancn <reg_info@qq.com>
2023-09-22 20:27:16 +0800

    remove comments

--- utils/log.go
+++ utils/log.go
@@ -31,47 +31,6 @@ func LogFatal(msg string, keyvals ...interface{}) {
 	logPrint(LogLevelFatal, msg, keyvals...)
 }
 
-//// ParseArgsWithError  将可变参数转换成map, 其中有err关键字返回error
-//// @author Ryan Fan
-//// @param  variadic arguments, key/value键值对，可变参数个数必须为偶数
-//// @return 错误值
-//// @return 除错误值以外的其他参数key->value对
-//func ParseArgsWithError(keyvals ...interface{}) (error, map[string]interface{}) {
-//	countArgs := len(keyvals)
-//	// 如果可变参数个数为0，肯定没有error
-//	if countArgs == 0 {
-//		return nil, nil
-//	}
-//
-//	var errValue error
-//	args := make(map[string]interface{})
-//	for i := 0; i < countArgs; i = i + 2 {
-//		// 如果下一个值的index小于参数个数，继续进行判断
-//		if i+1 < countArgs {
-//			// 第i个值作为map的key, 第i+1个值作为map的value
-//			k, ok := keyvals[i].(string)
-//			if !ok {
-//				continue
-//			}
-//
-//			switch k {
-//			case "err", "error":
-//				v, ok := keyvals[i+1].(error)
-//				if ok {
-//					errValue = v
-//				} else {
-//					// if next value is not an error, try convert it's string representation to error
-//					errValue = errors.New(fmt.Sprintf("%v", keyvals[i+1]))
-//				}
-//			default:
-//				args[k] = keyvals[i+1]
-//			}
-//		}
-//	}
-//
-//	return errValue, args
-//}
-
 // ParseArgs  解析error和message用统一格式展示出来
 func ParseArgs(keyvals ...interface{}) (string, error, map[string]interface{}) {
 	countArgs := len(keyvals)
