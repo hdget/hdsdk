bca9e29c3ef2eef8323392c792f7cc8364337c1a
rfancn <reg_info@qq.com>
2024-05-27 17:35:55 +0800

    rewrite get event function

--- lib/dapr/module_event_handler.go
+++ lib/dapr/module_event_handler.go
@@ -5,6 +5,7 @@ import (
 	"github.com/dapr/go-sdk/service/common"
 	"github.com/hdget/hdsdk/v2"
 	"github.com/hdget/hdutils"
+	"github.com/pkg/errors"
 	"time"
 )
 
@@ -19,11 +20,6 @@ type eventHandlerImpl struct {
 	fn     EventFunction // 调用函数
 }
 
-type handleResult struct {
-	retry bool
-	err   error
-}
-
 type EventFunction func(ctx context.Context, event *common.TopicEvent) (retry bool, err error)
 
 func (h eventHandlerImpl) GetTopic() string {
@@ -32,36 +28,25 @@ func (h eventHandlerImpl) GetTopic() string {
 
 func (h eventHandlerImpl) GetEventFunction() common.TopicEventHandler {
 	return func(ctx context.Context, event *common.TopicEvent) (bool, error) {
-		// 在go routine中执行具体的函数
-		quit := make(chan *handleResult, 1)
-		go func(quit chan *handleResult) {
-			var retry bool
-			var err error
-			// 挂载defer函数
-			defer func() {
-				if r := recover(); r != nil {
-					hdutils.RecordErrorStack(h.module.GetApp())
-				}
-				quit <- &handleResult{
-					retry: retry,
-					err:   err,
-				}
-			}()
-
-			// 执行具体的函数
-			retry, err = h.fn(ctx, event)
-		}(quit)
-
-		select {
-		case <-time.After(h.module.GetAckTimeout() - 1*time.Minute):
-			hdsdk.Logger().Error("event processing timeout", "module", h.module.GetMeta().StructName, "topic", h.GetTopic(), "handler", hdutils.Reflect().GetFuncName(h.fn), "message", trimData(event.RawData))
-			return false, nil // 丢弃消息不重试
-		case ret := <-quit:
-			if ret.err != nil {
-				hdsdk.Logger().Error("event processing done", "module", h.module.GetMeta().StructName, "topic", h.GetTopic(), "handler", hdutils.Reflect().GetFuncName(h.fn), "message", trimData(event.RawData), "err", ret.err)
+		defer func() {
+			if r := recover(); r != nil {
+				hdutils.RecordErrorStack(h.module.GetApp())
 			}
-			return ret.retry, ret.err
+		}()
+
+		ctx, cancel := context.WithTimeout(ctx, h.module.GetAckTimeout()-1*time.Minute)
+		defer cancel()
+
+		// 执行具体的函数
+		retry, err := h.fn(ctx, event)
+		if err != nil {
+			if errors.Is(err, context.DeadlineExceeded) {
+				hdsdk.Logger().Error("event processing timeout", "module", h.module.GetMeta().StructName, "topic", h.GetTopic(), "handler", hdutils.Reflect().GetFuncName(h.fn), "message", trimData(event.RawData))
+				return false, nil
+			}
+			hdsdk.Logger().Error("event processing", "module", h.module.GetMeta().StructName, "topic", h.GetTopic(), "handler", hdutils.Reflect().GetFuncName(h.fn), "message", trimData(event.RawData))
 		}
+		return retry, err
 	}
 }
 
