19753ae1788e4e917c7ab929cd6bd1a381fe5f42
rfancn <reg_info@qq.com>
2024-06-12 14:06:37 +0800

    remove unused err definition

--- errdef/errdef.go
+++ errdef/errdef.go
@@ -3,13 +3,8 @@ package errdef
 import "github.com/pkg/errors"
 
 var (
-	ErrConfigProviderNotFound = errors.New("config provider not found")
 	ErrConfigProviderNotReady = errors.New("config provider not ready")
-	ErrEmptyProvider          = errors.New("empty provider")
 	ErrInvalidCapability      = errors.New("invalid capability")
 	ErrInvalidConfig          = errors.New("invalid config")
 	ErrEmptyConfig            = errors.New("empty config")
-	ErrEmptyDb                = errors.New("empty db")
-	ErrEmptyDbBuilder         = errors.New("no db builder specified")
-	ErrValueNotSettable       = errors.New("value is not settable")
 )
