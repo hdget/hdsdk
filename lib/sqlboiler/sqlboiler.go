ed86f6f92b08d9cd43bc304e352bf16674648be0
rfancn <reg_info@qq.com>
2024-06-27 16:56:45 +0800

    add IfNull in sqlboiler

--- lib/sqlboiler/sqlboiler.go
+++ lib/sqlboiler/sqlboiler.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 	"github.com/hdget/hdsdk/v2/lib/pagination"
 	"github.com/hdget/hdsdk/v2/protobuf"
+	jsonUtils "github.com/hdget/hdutils/json"
 	"github.com/volatiletech/sqlboiler/v4/boil"
 	"github.com/volatiletech/sqlboiler/v4/queries/qm"
+	"reflect"
 	"time"
 )
 
@@ -15,23 +17,60 @@ func GetLimitQueryMods(list *protobuf.ListParam) []qm.QueryMod {
 	return []qm.QueryMod{qm.Offset(int(p.Offset)), qm.Limit(int(p.PageSize))}
 }
 
-// IfNullZeroString 如果传了args则用args[0]做为alias, 否则就用oldValue做为alias
-func IfNullZeroString(oldValue string, args ...string) string {
-	alias := oldValue
+func IfNull(column string, zeroValue any, args ...string) string {
+	alias := column
 	if len(args) > 0 {
 		alias = args[0]
 	}
-	return fmt.Sprintf("IFNULL((%s), '') AS \"%s\"", oldValue, alias)
+
+	if reflect.ValueOf(zeroValue).IsZero() {
+		switch v := zeroValue.(type) {
+		case string:
+			if v == "" {
+				return fmt.Sprintf("IFNULL((%s), '') AS \"%s\"", column, alias)
+			}
+		case int8, int, int32, int64, uint8, uint, uint32, uint64:
+			return fmt.Sprintf("IFNULL((%s), 0) AS \"%s\"", column, alias)
+		case float32, float64:
+			return fmt.Sprintf("IFNULL((%s), 0.0) AS \"%s\"", column, alias)
+		case []byte:
+			if jsonUtils.IsEmptyJsonObject(v) {
+				return fmt.Sprintf("IFNULL((%s), '{}') AS \"%s\"", column, alias)
+			} else if jsonUtils.IsEmptyJsonArray(v) {
+				return fmt.Sprintf("IFNULL((%s), '[]') AS \"%s\"", column, alias)
+			}
+		}
+	}
+
+	return fmt.Sprintf("IFNULL((%s), %v) AS \"%s\"", column, zeroValue, alias)
 }
 
-// IfNullZeroNumber 如果传了args则用args[0]做为alias, 否则就用oldValue做为alias
-func IfNullZeroNumber(oldValue string, args ...string) string {
-	alias := oldValue
-	if len(args) > 0 {
-		alias = args[0]
-	}
-	return fmt.Sprintf("IFNULL((%s), 0) AS \"%s\"", oldValue, alias)
-}
+//// IfNullZeroString 如果传了args则用args[0]做为alias, 否则就用column做为alias
+//func IfNullZeroString(column string, args ...string) string {
+//	alias := column
+//	if len(args) > 0 {
+//		alias = args[0]
+//	}
+//	return fmt.Sprintf("IFNULL((%s), '') AS \"%s\"", column, alias)
+//}
+//
+//// IfNullZeroNumber 如果传了args则用args[0]做为alias, 否则就用oldValue做为alias
+//func IfNullZeroNumber(column string, args ...string) string {
+//	alias := column
+//	if len(args) > 0 {
+//		alias = args[0]
+//	}
+//	return fmt.Sprintf("IFNULL((%s), 0) AS \"%s\"", column, alias)
+//}
+//
+//// IfNullJsonObject 如果传了args则用args[0]做为alias, 否则就用column做为alias
+//func IfNullJsonObject(column string, args ...string) string {
+//	alias := column
+//	if len(args) > 0 {
+//		alias = args[0]
+//	}
+//	return fmt.Sprintf("IFNULL((%s), 0) AS \"%s\"", column, alias)
+//}
 
 // WithUpdateTime 除了cols中的会更新以外还会更新更新时间字段
 func WithUpdateTime(cols map[string]any, args ...string) map[string]any {
@@ -56,5 +95,5 @@ func GetDB(args ...boil.Executor) boil.Executor {
 }
 
 func SUM(col string, args ...string) string {
-	return IfNullZeroNumber(fmt.Sprintf("SUM(%s)", col), args...)
+	return IfNull(fmt.Sprintf("SUM(%s)", col), 0, args...)
 }
