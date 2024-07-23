9f6e6346ff86bf4ba791a9a69b4fe0910c0c6756
rfancn <reg_info@qq.com>
2024-07-23 18:44:43 +0800

    optimize verify logic

--- lib/captcha/store.go
+++ lib/captcha/store.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/hdget/hdsdk/v2"
 	"github.com/pkg/errors"
+	"github.com/spf13/cast"
 )
 
 type redisCaptchaStore struct {
@@ -11,7 +12,18 @@ type redisCaptchaStore struct {
 }
 
 const (
-	captchaRedisKeyTemplate = "captcha:%s"
+	maxFailures                = 3
+	captchaRedisKeyTemplate    = "captcha:%s"
+	luaCaptchaIncreaseFailures = `
+local key = KEYS[1]
+local maxFailures = tonumber(ARGV[1])
+local failures = tonumber(redis.call('HGET', key, "failures"))
+if failures < maxFailures-1 then
+	redis.call('HMSET', key, "failures", failures+1)
+else
+	redis.call('DEL', key)
+end;
+`
 )
 
 func Store(args ...string) CaptchaStore {
@@ -26,15 +38,24 @@ func Store(args ...string) CaptchaStore {
 }
 
 func (r redisCaptchaStore) Set(captchaId string, captchaValue string, expires int) error {
-	err := hdsdk.Redis().My().SetEx(r.getStoreKey(captchaId), r.getStoreValue(captchaValue), expires)
+	err := hdsdk.Redis().My().HMSet(r.getStoreKey(captchaId), map[string]interface{}{
+		"value":    r.getStoreValue(captchaValue),
+		"failures": 0,
+	})
 	if err != nil {
 		return errors.Wrap(err, "store set captcha")
 	}
+
+	err = hdsdk.Redis().My().Expire(r.getStoreKey(captchaId), expires)
+	if err != nil {
+		return errors.Wrap(err, "store expire captcha")
+	}
+
 	return nil
 }
 
 func (r redisCaptchaStore) Get(captchaId string, clear bool) (string, error) {
-	val, err := hdsdk.Redis().My().GetString(r.getStoreKey(captchaId))
+	val, err := hdsdk.Redis().My().HGet(r.getStoreKey(captchaId), "value")
 	if err != nil {
 		return "", errors.Wrap(err, "store get captcha")
 	}
@@ -45,24 +66,29 @@ func (r redisCaptchaStore) Get(captchaId string, clear bool) (string, error) {
 			return "", errors.Wrap(err, "store clear captcha")
 		}
 	}
-	return val, nil
+	return cast.ToString(val), nil
 }
 
 func (r redisCaptchaStore) Verify(captchaId, answer string, clear bool) (bool, error) {
-	if answer == "" {
-		return false, errors.New("empty answer")
-	}
-
 	storeValue, err := r.Get(captchaId, clear)
 	if err != nil {
 		return false, err
 	}
 
-	if storeValue == "" {
-		return false, errors.New("empty store value")
+	verified := false
+	if storeValue != "" && r.getStoreValue(answer) == storeValue {
+		verified = true
 	}
 
-	return r.getStoreValue(answer) == storeValue, nil
+	if !verified {
+		if !clear {
+			err = r.increaseFailures(captchaId)
+			if err != nil {
+				return false, err
+			}
+		}
+	}
+	return verified, nil
 }
 
 func (r redisCaptchaStore) getStoreKey(id string) string {
@@ -71,4 +97,12 @@ func (r redisCaptchaStore) getStoreKey(id string) string {
 
 func (r redisCaptchaStore) getStoreValue(captchaValue string) string {
 	return r.generator + captchaValue
+}
+
+func (r redisCaptchaStore) increaseFailures(captchaId string) error {
+	_, err := hdsdk.Redis().My().Eval(luaCaptchaIncreaseFailures, []any{r.getStoreKey(captchaId)}, []any{maxFailures})
+	if err != nil {
+		return errors.Wrap(err, "store increase captcha")
+	}
+	return nil
 }
