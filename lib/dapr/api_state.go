5c303c6cbebb74cbed28ca7feae43baacae2ab5b
rfancn <reg_info@qq.com>
2024-10-14 16:35:10 +0800

    add dapr GetBulkState

--- lib/dapr/api_state.go
+++ lib/dapr/api_state.go
@@ -2,9 +2,11 @@ package dapr
 
 import (
 	"context"
+	"fmt"
 	"github.com/dapr/go-sdk/client"
 	"github.com/hdget/hdutils/convert"
 	"github.com/pkg/errors"
+	"github.com/spf13/cast"
 )
 
 // SaveState 保存状态
@@ -33,7 +35,7 @@ func (a apiImpl) SaveState(storeName, key string, value interface{}) error {
 	return nil
 }
 
-// GetState 保存状态
+// GetState 获取状态
 func (a apiImpl) GetState(storeName, key string) ([]byte, error) {
 	daprClient, err := client.NewClient()
 	if err != nil {
@@ -44,13 +46,42 @@ func (a apiImpl) GetState(storeName, key string) ([]byte, error) {
 	}
 
 	// IMPORTANT: daprClient是全局的连接, 不能关闭
-	//defer daprClient.Close()
 	item, err := daprClient.GetState(context.Background(), storeName, key, nil)
 	if err != nil {
 		return nil, errors.Wrapf(err, "get state, store: %s, key: %s", storeName, key)
 	}
 
 	return item.Value, nil
+}
+
+// GetBulkState 批量获取状态
+func (a apiImpl) GetBulkState(storeName string, keys any) (map[string][]byte, error) {
+	strKeys, err := cast.ToStringSliceE(keys)
+	if err != nil {
+		return nil, fmt.Errorf("invalid keys, keys: %v", keys)
+	}
+
+	daprClient, err := client.NewClient()
+	if err != nil {
+		return nil, errors.Wrap(err, "new dapr client")
+	}
+	if daprClient == nil {
+		return nil, errors.New("dapr client is null, name resolution service may not started, please check it")
+	}
+
+	// IMPORTANT: daprClient是全局的连接, 不能关闭
+	items, err := daprClient.GetBulkState(context.Background(), storeName, strKeys, nil, 100)
+	if err != nil {
+		return nil, errors.Wrapf(err, "get bulk state, store: %s, keys: %s", storeName, keys)
+	}
+
+	results := make(map[string][]byte, len(items))
+	for _, item := range items {
+		if item.Error == "" {
+			results[item.Key] = item.Value
+		}
+	}
+	return results, nil
 }
 
 // DeleteState 删除状态
